700a547a372e871a99b1e7ba4e3857484ae072de
vikvik <ivanvikvik@gmail.com>
2022-10-11 11:45:40 +0300

    Initial commit

---
+++ src/by/itstep/vikvik/controller/Main.java
@@ -0,0 +1,17 @@
+package by.itstep.vikvik.controller;
+
+import by.itstep.vikvik.model.entity.Student;
+
+class A {
+
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Student student = new Student();
+        A a = new A();
+
+        Math math = new Math();
+
+    }
+}
---
+++ src/by/itstep/vikvik/view/Printer.java
@@ -0,0 +1,7 @@
+package by.itstep.vikvik.view;
+
+public class Printer {
+    protected static void print(String msg) {
+        System.out.print(msg);
+    }
+}
